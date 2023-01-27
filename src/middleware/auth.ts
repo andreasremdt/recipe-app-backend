6c41d8c0b50227f2adca49242b2d88208f690060
Andreas Remdt <me@andreasremdt.com>
2023-01-27 09:24:03 +0100

    feat: check if user exists in db in auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,8 +3,9 @@ import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 import config from "../config";
+import prisma from "../database/client";
 
-export default function auth(req: Request, res: Response, next: NextFunction) {
+export default async function auth(req: Request, res: Response, next: NextFunction) {
   const bearer = req.headers.authorization;
 
   try {
@@ -22,6 +23,16 @@ export default function auth(req: Request, res: Response, next: NextFunction) {
       User,
       "id" | "email" | "name"
     >;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new Error("Not authorized.");
+    }
 
     req.user = { email, name, id };
     next();
