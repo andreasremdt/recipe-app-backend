091826efb052a603222832cc44c2deab5d08364c
Andreas Remdt <me@andreasremdt.com>
2023-01-27 10:59:13 +0100

    feat: add generic 404 handler

--- src/router.ts
+++ src/router.ts
@@ -9,6 +9,7 @@ import auth from "./middleware/auth";
 
 export default express
   .Router()
+
   .post("/auth/login", validate(userSchema), user.login)
   .post("/auth/signup", validate(userSchema), user.signup)
   .post("/auth/logout", user.logout)
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 
 import router from "./router";
 import handleError from "./middleware/handle-error";
+import handleNotFound from "./middleware/not-found";
 
 export default express()
   .disable("x-powered-by")
@@ -12,4 +13,5 @@ export default express()
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use("/api", router)
+  .get("*", handleNotFound)
   .use(handleError);
