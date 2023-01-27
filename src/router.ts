f30e9111eba7d3d003f41d4c25c0dc49eb9cf879
Andreas Remdt <me@andreasremdt.com>
2023-01-27 09:24:25 +0100

    feat: validate request body for recipe endpoints

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import express from "express";
 import * as user from "./controllers/user";
 import * as recipe from "./controllers/recipe";
 import userSchema from "./schemas/user";
+import recipeSchema from "./schemas/recipe";
 import validate from "./middleware/validate";
 import auth from "./middleware/auth";
 
@@ -14,6 +15,6 @@ export default express
 
   .get("/recipes", auth, recipe.getAll)
   .get("/recipes/:id", auth, recipe.get)
-  .post("/recipes", auth, recipe.create)
-  .patch("/recipes/:id", auth, recipe.update)
+  .post("/recipes", auth, validate(recipeSchema), recipe.create)
+  .patch("/recipes/:id", auth, validate(recipeSchema), recipe.update)
   .delete("/recipes/:id", auth, recipe.destroy);
