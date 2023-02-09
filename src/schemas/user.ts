2942f63d922544784553492bec12f3c3ffb6e839
Andreas Remdt <me@andreasremdt.com>
2023-02-09 13:32:13 +0100

    fix: allow empty string as user name

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,7 +1,7 @@
 import joi from "joi";
 
 export default joi.object({
-  name: joi.string(),
+  name: joi.string().allow(""),
   email: joi.string().email().required(),
   password: joi.string().min(8).required(),
 });
