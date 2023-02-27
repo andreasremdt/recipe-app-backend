d501e377be92e3dd44093aba5f9408571e2b0d12
Andreas Remdt <me@andreasremdt.com>
2023-02-27 10:23:01 +0100

    fix: allow empty strings in schema

--- src/schemas/recipe.ts
+++ src/schemas/recipe.ts
@@ -2,10 +2,10 @@ import joi from "joi";
 
 export default joi.object({
   title: joi.string().max(255).required(),
-  description: joi.string(),
-  thumbnailUrl: joi.string(),
-  ingredients: joi.string(),
-  instructions: joi.string(),
+  description: joi.string().allow(""),
+  thumbnailUrl: joi.string().allow(""),
+  ingredients: joi.string().allow(""),
+  instructions: joi.string().allow(""),
   rating: joi.number().min(1).max(5),
   trashed: joi.boolean(),
   difficulty: joi.string(),
